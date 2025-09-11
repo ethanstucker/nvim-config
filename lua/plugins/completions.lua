c7e65900810450f5e5a83032e99e829373c47735
Ethan Stucker <ethan.stucker@proton.me>
2025-09-11 10:17:38 -0500

    Copilot Autocomplete and fixed cmp autocomplete

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,45 +1,67 @@
 return {
-	{
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter", -- Load cmp only when entering insert mode
+	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
 		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets", -- A good collection of snippets
+        "zbirenbaum/copilot-cmp",
 	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
+		-- Load snippets
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			-- Configure the appearance of the completion window
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			-- This is the crucial part that was missing
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Press Enter to confirm selection
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			-- These are the sources cmp will use for suggestions
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+                { name = "copilot" },
+			}),
+		})
+	end,
 }
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          -- This is the setting that enables the ghost text
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-l>", -- Use Ctrl+L to accept the suggestion
+            dismiss = "<C-h>", -- Use Ctrl+H to dismiss it
+          },
+        },
+        panel = {
+          enabled = false, -- Keep this false unless you want the side panel
+        },
+      })
+    end,
+  },
+  {
+    -- This plugin is the bridge between copilot and nvim-cmp
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+}
