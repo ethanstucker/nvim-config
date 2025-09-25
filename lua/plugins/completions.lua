6f1ff03c1caeea8d760eda397dcc8b025fab7740
Ethan Stucker <ethan.stucker@proton.me>
2025-09-25 08:29:10 -0500

    Made enter key smarter with nvim-cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,69 +1,75 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter", -- Load cmp only when entering insert mode
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		"rafamadriz/friendly-snippets", -- A good collection of snippets
-		"zbirenbaum/copilot-cmp",
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter", -- Load cmp only when entering insert mode
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets", -- A good collection of snippets
+        "zbirenbaum/copilot-cmp",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
 
-		-- Load snippets
-		require("luasnip.loaders.from_vscode").lazy_load()
+        -- Load snippets
+        require("luasnip.loaders.from_vscode").lazy_load()
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			-- Configure the appearance of the completion window
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			-- This is the crucial part that was missing
-			mapping = cmp.mapping.preset.insert({
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Press Enter to confirm selection
-				["<Tab>"] = cmp.mapping(function(fallback)
-					local copilot = require("copilot.suggestion")
-					if copilot.is_visible() then
-						copilot.accept()
-					elseif cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            -- Configure the appearance of the completion window
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            -- This is the crucial part that was missing
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() and cmp.get_selected_entry() then
+                        cmp.confirm({ select = true })
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    local copilot = require("copilot.suggestion")
+                    if copilot.is_visible() then
+                        copilot.accept()
+                    elseif cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
 
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			}),
-			-- These are the sources cmp will use for suggestions
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
-				{ name = "copilot" },
-			}),
-		})
-	end,
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            }),
+            -- These are the sources cmp will use for suggestions
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "buffer" },
+                { name = "path" },
+                { name = "copilot" },
+            }),
+        })
+    end,
 }
